4cddfd4077b56c4d8c6cb7a2a31bc126b4f0b256
EnnioEvo <artemisvii@gmail.com>
2020-03-31 12:28:09 +0200

    fix test columned set

--- Code Ennio/data_reordering_greg.py
+++ Code Ennio/data_reordering_greg.py
@@ -9,12 +9,9 @@ import math
 # "pid" e "age", che si possono togliere semplicemente cambiando il range di "ind2" nell'inner "for loop"
 
 # Data import from folder
-
 data_set_x_train = np.array(pd.read_csv("../data/data_train_clean_entire_dataset.csv"))
 data_set_y_train = np.array(pd.read_csv("../data/train_labels.csv"))
 data_set_x_test = np.array(pd.read_csv("../data/data_test_clean_entire_dataset.csv"))
-
-
 
 #Concatenation of all the data for the 12 hours in data_set_x_train to obtain one single raw for each patient
 N = data_set_x_train.shape[0] #227940
@@ -28,28 +25,21 @@ for ind1 in range(0, N, 12):
         X_temp=np.array([np.concatenate((X_temp, np.array([data_set_x_train[ind2][2:]])), axis=None)])
     X_train[int(ind1/12), :] = X_temp
 
-#X_train=X_train[1:, :]
-print(X_train.shape)
-
 
 #Concatenation of all the data for the 12 hours in data_set_x_test to obtain one single raw for each patient
 
-X_test=np.zeros((1,36+34*11))
+N = data_set_x_test.shape[0] #151968
+X_test=np.zeros((math.ceil(N/12),36+34*11))
 
-# put "15960" in the range function to stop the iteration of the inner "for cycle" exactly at the last row of tha last patient
-for ind1 in range(0, 15960, 12):
-    X_temp = np.array([data_set_x_train[ind1][0:2]])
+# put "151968" in the range function to stop the iteration of the inner "for cycle" exactly at the last row of tha last patient
+for ind1 in range(0, N, 12):
+    X_temp = np.array([data_set_x_test[ind1][0:2]])
     for ind2 in range(ind1,ind1+12):
         X_temp=np.array([np.concatenate((X_temp, np.array([data_set_x_test[ind2][2:]])), axis=None)])
-    X_test=np.row_stack((X_test,X_temp))
-
-X_test=X_test[1:, :]
-print(X_test.shape)
+    X_test[int(ind1/12), :] = X_temp
 
 #example of just one label
 Y_train = np.array([data_set_y_train[:, 1]]).T
-print(Y_train)
-print(Y_train.shape)
 
 # these labels are useful for saving back arrays to dataframes
 labels = ['EtCO2', 'PTT', 'BUN', 'Lactate', 'Temp', 'Hgb', 'HCO3', 'BaseExcess',
