4bc61a1b441b36a8e1c8b6d4b7d350777af96485
EnnioEvo <artemisvii@gmail.com>
2020-03-31 12:10:29 +0200

    .

--- Code Alberto/data_cleaning_all.py
+++ Code Alberto/data_cleaning_all.py
@@ -6,8 +6,8 @@ from progress.bar import IncrementalBar
 
 
 # Data import:
-train_features = pd.read_csv("../data/test_features.csv")
-#test_features = pd.read_csv("../data/test_features.csv")
+train_features = pd.read_csv("../data/train_features.csv")
+test_features = pd.read_csv("../data/test_features.csv")
 train_labels = pd.read_csv("../data/train_labels.csv")
 
 # Extracting information:
@@ -113,4 +113,4 @@ print('## CHECK COMPLETE ##')
 submSet = pd.DataFrame(data_set_clean, index=None, columns=col)
 print(submSet.head())
 print("Saved file shape: ", submSet.shape)
-submSet.to_csv('../data/data_test_clean_entire_dataset.csv', header=True, index=False)
+submSet.to_csv('../data/data_train_clean_entire_dataset.csv', header=True, index=False)
