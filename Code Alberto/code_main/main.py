01a62be1b17ad18fa681663d92f70221955aca1c
acenedese <acenedese@student.ethz.ch>
2020-04-05 19:24:24 +0200

    feature selection task2

--- Code Alberto/code_main/main.py
+++ Code Alberto/code_main/main.py
@@ -1,3 +1,22 @@
 import numpy as np
 import pandas as pd
 
+train_features = pd.read_csv('data/train_features_clean_all_no_norm.csv')
+
+# Informatons on the headers:
+patient_characteristics = ["Age"] 
+time_car = ['Time']
+vital_signs = ["Heartrate", "SpO2", "ABPs", "ABPm", "ABPd", "RRate", 'Temp']
+tests = ['EtCO2', 'PTT', 'BUN', 'Lactate', 'Hgb', 'HCO3', 'BaseExcess',
+       'Fibrinogen', 'Phosphate', 'WBC', 'Creatinine', 'PaCO2', 'AST', 'FiO2',
+       'Platelets', 'SaO2', 'Glucose', 'Magnesium', 'Potassium', 'Calcium',
+       'Alkalinephos', 'Bilirubin_direct', 'Chloride', 'Hct',
+       'Bilirubin_total', 'TroponinI', 'pH']
+labels_tests = ['LABEL_BaseExcess', 'LABEL_Fibrinogen', 'LABEL_AST',
+       'LABEL_Alkalinephos', 'LABEL_Bilirubin_total', 'LABEL_Lactate',
+       'LABEL_TroponinI', 'LABEL_SaO2', 'LABEL_Bilirubin_direct',
+       'LABEL_EtCO2' ]
+labels_sepsis = ['LABEL_Sepsis']
+labels_VS_mean = ['LABEL_RRate', 'LABEL_ABPm', 'LABEL_SpO2', 'LABEL_Heartrate']
+
+####### TASK 1 #######
